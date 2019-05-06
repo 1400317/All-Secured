c0317149a1b1867b37420c94f778bd615dc7dfa0
Ebtesam-A <50187676+Ebtesam-A@users.noreply.github.com>
2019-05-06 19:11:59 +0200

    Update All-secure.py

--- All-secure.py
+++ All-secure.py
@@ -18,3 +18,23 @@ x = int (y)
 persons =[]
 encoding_persons= []
 i=0
+
+
+                            #training
+while (i<x):
+    #get the training img location from the user
+    photo = input ("the location of pic of person%d: " %i)
+    face = photo.replace('\\' , '/')
+    #solved the problem of recieving the escape char '\' in the location input by the user
+    
+    #reading my pic.& adding it to a list
+    person = face_recognition.load_image_file(face)
+    persons.append (person)
+    
+    #encoding face features from my pic. & adding it to a list
+    encoding_person = face_recognition.face_encodings(person)[0]
+    encoding_persons.append (encoding_person)
+   
+    i = i+1
+   
+    
