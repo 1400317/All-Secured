5f1e6419d314658f129870928b51fd5d8d59ca00
SalmaBayomy <50339499+SalmaBayomy@users.noreply.github.com>
2019-05-06 22:12:44 +0200

    Update Door.ino

--- Door.ino
+++ Door.ino
@@ -6,3 +6,19 @@ void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
   Door.attach(5); //Attach Vertical Servo to Pin 5
+  Door.write(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available() > 0) {
+    char data = Serial.parseInt();
+        
+    if (data == 0){
+       Door.write (0);
+    }
+    else{
+       Door.write (90);
+    }
+}
+}
